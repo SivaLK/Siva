865748781ed0a418714e47ad26a937c3261fda13
SivaLK <35689027+SivaLK@users.noreply.github.com>
2018-02-19 16:06:17 +0530

    multiples of five

---
+++ multiples of five 19-2-18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i;
+    printf("Enter thee number ");
+    scanf("%d",&a);
+    for(b=1;b<=10;b++)
+{
+	i=a*b;
+printf("The values are %d\n",i);
+}
+	return 0;
+}
